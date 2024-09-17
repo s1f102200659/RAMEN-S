0e4619c7e9bcc7a9dd353e668435facc2b656bc8
nyaa0422 <arousalofneetysk6@gmail.com>
2024-09-17 16:00:38 +0900

    請求履歴の画面作りました

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/',views.home, name='home')
+    path('home/',views.home, name='home'),
+    path('billing_history/', views.billing_history, name='billing_history'),
 ]
