94e3dfa6168e5e0d02aad2f493fef34c70c20454
INIAD <s1f102200659@iniad.org>
2024-09-17 15:18:05 +0900

    ホームページのレイアウトを作成

--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     return HttpResponse("Hello, Django!")
+
+def home(request):
+    return render(request, 'app/home.html')
