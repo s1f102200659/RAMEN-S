238b3a1aa59d53d815dd5ae8a83d87efe42a8308
Sonshi04 <arousalofneetysk6@gmail.com>
2024-09-17 14:48:51 +0900

    test negoya

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import top_view
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('top/', top_view, name='top'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, Django!")
+
+from django.shortcuts import render
+
+def top_view(request):
+    return render(request, 'top.html')
