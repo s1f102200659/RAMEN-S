31cbc95d53ed81c7df58bc64516276d281da7b13
INIAD <s1f102200659@iniad.org>
2024-09-18 12:07:31 +0900

    管理サイトにデータベースを登録

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Invoice, Payment  # モデルをインポート
 
-# Register your models here.
+# モデルを管理画面に登録
+admin.site.register(User)
+admin.site.register(Invoice)
+admin.site.register(Payment)
